dac6edb434f421e28ff452576c7f363fe861e1f5
Klar <klar@livinglogic.de>
2026-04-28 09:48:49 +0000

    Klar Auto-Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ErrorBusProvider } from '@/components/ErrorBus';
 import { Layout } from '@/components/Layout';
 import DashboardOverview from '@/pages/DashboardOverview';
-import { WorkflowPlaceholders } from '@/components/WorkflowPlaceholders';
 import AdminPage from '@/pages/AdminPage';
 import MitarbeiterPage from '@/pages/MitarbeiterPage';
 import SchichttypenPage from '@/pages/SchichttypenPage';
@@ -32,7 +31,7 @@ export default function App() {
               {/* <public:routes> */}
               {/* </public:routes> */}
               <Route element={<Layout />}>
-                <Route index element={<><div className="mb-8"><WorkflowPlaceholders /></div><DashboardOverview /></>} />
+                <Route index element={<DashboardOverview />} />
                 <Route path="mitarbeiter" element={<MitarbeiterPage />} />
                 <Route path="schichttypen" element={<SchichttypenPage />} />
                 <Route path="schichtplan" element={<SchichtplanPage />} />
